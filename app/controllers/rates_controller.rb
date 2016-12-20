ad36a9a0a225e8a29b68b448e7709fb9f6c32f9e
Elliott <elmelville@hotmail.com>
2016-12-20 11:16:19 +1000

    blah

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -57,13 +57,13 @@ class RatesController < ShopifyApp::AuthenticatedController
   end
 
   protected
-    def set_csrf_headers
-      if request.xhr?
-        # Add the newly created csrf token to the page headers
-        # These values are sent on 1 request only
-        response.headers['X-CSRF-Token'] = "#{form_authenticity_token}"
-        response.headers['X-CSRF-Param'] = "#{request_forgery_protection_token}"
-      end
+  
+  def set_csrf_headers
+    if request.xhr?
+      # Add the newly created csrf token to the page headers
+      # These values are sent on 1 request only
+      response.headers['X-CSRF-Token'] = "#{form_authenticity_token}"
+      response.headers['X-CSRF-Param'] = "#{request_forgery_protection_token}"
     end
-  end  
+  end
 end
